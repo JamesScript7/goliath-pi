e082f154ad17498566bcdaf8f66c46053a5ca0ad
James Kim <yameskim@gmail.com>
2019-12-15 22:13:15 -0500

    eslint

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
 app.listen(4000, () => {
-    console.log('Example app listening!');
+  console.log('Example app listening!');
 });
