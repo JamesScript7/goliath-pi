9a46d553a74b04eadaf6224c3d859bc444a6ab1e
James Kim <yameskim@gmail.com>
2020-01-19 17:30:18 -0500

    prep weather component

---
+++ src/components/WeatherReport.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="weatherreport">
+      <p>Weather Report</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WeatherReport',
+  props: {
+    imgUrl: String,
+  },
+};
+</script>
+
+<style scoped>
+.weather {
+    color: darkcyan;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,13 @@
     <Wallpaper v-bind:imgUrl="imgUrl" />
     <!-- <CureatrDailyReport /> -->
     <DateAndTime v-bind:moment="moment" />
-    <!-- <WeatherReport /> -->
+    <WeatherReport />
   </div>
 </template>
 
 // TODO:
 // Add Cureatr logo
-// Wire up WeatherReport component
+// WeatherReport client and serverside
 
 <script>
 import axios from 'axios';
@@ -18,11 +18,13 @@ import moment from 'moment';
 import Wallpaper from '@/components/Wallpaper.vue';
 // import CureatrDailyReport from '@/components/CureatrDailyReport.vue';
 import DateAndTime from '@/components/DateAndTime.vue';
-// import Weather from '@/components/Weather.vue';
+import WeatherReport from '@/components/WeatherReport.vue';
 
 const ONE_SECOND = 1000;
-const PHOTO_REFRESH_INTERVAL = 20000;
+const THIRTY_MINUTES = (ONE_SECOND * 60) * 30;
+const PHOTO_REFRESH_INTERVAL = (ONE_SECOND * 60) * 5;
 const RANDOM_PHOTO_API_URL = 'http://localhost:4000/random-photo';
+const WEATHER_REPORT_API_URL = 'http://localhost:4000/weather-report';
 
 export default {
   name: 'home',
@@ -30,7 +32,7 @@ export default {
     Wallpaper,
     // CureatrDailyReport
     DateAndTime,
-    // WeatherReport
+    WeatherReport,
   },
   data() {
     return {
@@ -54,6 +56,17 @@ export default {
           this.imgUrl = response.data.urls.full;
         });
     },
+    grabWeatherReport() {
+      console.log('grabWeatherReport');
+
+      axios.get(WEATHER_REPORT_API_URL)
+        .then((response) => {
+          console.log(response);
+          // if status is ok check?
+          // NOTE: prep the data better?
+          // this.imgUrl = response.data.urls.full;
+        });
+    },
     photoCountdown() {
       console.log('photoCountdown');
 
@@ -72,6 +85,15 @@ export default {
           this.moment.time = currentTime;
         }
       }, ONE_SECOND);
+    },
+    updateWeather() {
+      console.log('updateWeather');
+
+      setInterval(() => {
+        console.log('updateWeather setInterval');
+        // TODO:
+        // Updates weather every 30 minutes?
+      }, THIRTY_MINUTES);
     },
   },
   created() {
